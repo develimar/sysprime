da5374640638260a4ca379b462dc0edf3edb4f58
Prime_Midia <elimar.pc@gmail.com>
2023-04-06 16:44:39 -0300

    Primeiro Commit Projeto SysPrime

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,26 @@
+@extends('admin.layouts.master')
+
+@section('title')
+    SysPrime - Dashboard
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('title_page_ant')
+    Home
+@endsection
+
+@section('title_page_atu')
+    Dashboard
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('scripts')
+
+@endsection
+
